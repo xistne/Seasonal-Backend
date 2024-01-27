5011909c29d3c067ffad41ead8593f3ecdd338a3
xistne <du20063@gmail.com>
2024-01-27 22:38:18 +0900

    first commit

---
+++ src/main/java/com/springboot/seasonal/SeasonalApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.seasonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasonalApplication.class, args);
+	}
+
+}
